c11f5969bf36a4038e4232333434c0400fe29164
wkdckd41 <wkdckd41@gmail.com>
2024-03-12 00:46:50 +0900

    도메인 분석 설계 - 엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -13,13 +13,14 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "item_id")
     private Item item;
 
     @ManyToOne
-    @Column(name = "order_id")
+    @JoinColumn(name = "order_id")
     private Order order;
 
     private int orderPrice; // 주문 가격
-
     private int count; // 주문 수량
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain.item;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 단일 테이블 전략
+@DiscriminatorColumn(name = "dtype") // 구분 컬럼
 @Getter @Setter
 public abstract class Item {
 
@@ -18,4 +21,7 @@ public abstract class Item {
     private String name; // 이름
     private int price; // 가격
     private int stockQuantity; // 재고수량
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
