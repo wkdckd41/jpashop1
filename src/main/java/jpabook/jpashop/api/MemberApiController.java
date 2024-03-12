c8b2fa373e69083ed509fba67c6eceb1e6f60001
wkdckd41 <wkdckd41@gmail.com>
2024-03-13 06:43:11 +0900

    API 개발 기본 - 회원등록 API

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // @Controller + @ResponseBody
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    } // API 요청 값으로 Member 엔티티를 직접 받는다. 엔티티를 RequestBody에 직접 매핑하는 것은 좋지 않다.
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    } // API 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다. DTO를 엔티티로 변환하거나 엔티티를 DTO로 변환하는 로직이 추가되었다.
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
