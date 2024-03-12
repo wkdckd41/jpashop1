484095e3eaebb9fb4babddde3b287d41b14a1cf5
wkdckd41 <wkdckd41@gmail.com>
2024-03-13 03:00:16 +0900

    웹 계층 개발 - 변경 감지와 병합(merge)

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -59,17 +59,10 @@ public class ItemController {
     }
 
     @PostMapping("/items/{itemId}/edit")
-    public String updateItem(@PathVariable String itemId, @ModelAttribute("form") BookFrom form) {
-        // @ModelAttribute("form")을 사용하여 form 객체를 생성하고, form 객체에 요청 파라미터의 값을 바인딩한다.
-        Book book = new Book();
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookFrom form) {
 
-        itemService.saveItem(book);
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
+
         return "redirect:/items";
     }
 }
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -21,6 +21,15 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    // 상품 수정
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+    } // 변경 감지 기능을 사용하여 데이터를 수정
+
     // 상품 조회
     public List<Item> findItems() { // 상품 전체 조회
         return itemRepository.findAll();
